73a9d1777a1fa088cd0c10cd6abdabda4751d421
autumn-ragland <raglanda998@gmail.com>
2020-04-14 09:04:41 -0500

    set up complete

--- routes/api.js
+++ routes/api.js
@@ -2,54 +2,34 @@ let express = require('express');
 let router = express.Router();
 router.use(express.json())
 
-let EntryCollection = require('../models/EntrySchema')
-
 // test route
-// router.get('/test', (req,res) => {
-//     res.send("Test working");
-// });
+router.get('/test', (req,res) => {
+    res.send("Test working");
+});
 
 // get all  entries
 router.get('/', (req,res) => {
-    // res.send("All Entries");
-    EntryCollection.find(
-        {}, (error, result) => {
-            error ? res.send(error) : res.send(result)
-        }
-    );
+    res.send("All Entries");
 });
 
 // get entry by title
 router.get('/:entryTitle', (req,res) => {
-    // res.send("Entry By Title");
-    EntryCollection.findOne(
-        {entryTitle : req.params.entryTitle}, (error, result) => {
-            error ? res.send(error) : res.send(result)
-        }
-    );
+    res.send("Entry By Title");
 });
 
 // create entry
 router.post('/', (req,res) => {
-    // res.send("Create Entry");
-    EntryCollection.create(req.body);
-    res.send(req.body);
+    res.send("Create Entry");
 });
 
 // update entry
 router.put('/:entryTitle', (req,res) => {
-    // res.send("Update Entry");
-    EntryCollection.findOneAndUpdate({entryTitle : req.params.entryTitle}, req.body, (error, result) => {
-        error ? res.send(error) : res.send(`${req.params.entryTitle} Updated`);
-    })
+    res.send("Update Entry");
 });
 
 // delete entry
 router.delete('/:entryTitle', (req,res) => {
-    // res.send("Delete Entry");
-    EntryCollection.findOneAndDelete({entryTitle : req.params.entryTitle}, req.body, (error, result) => {
-        error ? res.send(error) : res.send(`${req.params.entryTitle} Deleted`);
-    });
+    res.send("Delete Entry");
 });
 
 module.exports = router;
